d2c30883adef0b5efded019f95d2b74b1c1f7f0c
Inuka-G <156221307+Inuka-G@users.noreply.github.com>
2025-03-05 23:16:46 +0530

    modules

--- test/invariant/Invariant.t.sol
+++ test/invariant/Invariant.t.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.18;
 
-import { Test } from "forge-std/";
+import { Test } from "forge-std/Test.sol";
+import {StdInvariant} from "forge "
 
 contract InvariantFuzzTswap { }
